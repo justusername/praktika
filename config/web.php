2f871d4211a5b941c37dbdee3d9a718f318ba9ea
justusername <infernomalinin@gmail.com>
2016-07-12 08:56:23 +0300

    changes

--- config/web.php
+++ config/web.php
@@ -9,7 +9,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'muchKeyVerySecret1',
+            'cookieValidationKey' => 'nU1sT45_yKNw0PL4hjibKMp1LtXWxHwy',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -38,14 +38,14 @@ $config = [
             ],
         ],
         'db' => require(__DIR__ . '/db.php'),
-        /*
+
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
+        
     ],
     'params' => $params,
 ];
