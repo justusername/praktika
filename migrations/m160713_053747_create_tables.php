da0e89165abe42c74b10c1ed882a9a2722098885
ArthurOvanesian <young_arty@bk.ru>
2016-07-13 09:24:11 +0300

    Создание таблиц с помощью миграции

---
+++ migrations/m160713_053747_create_tables.php
@@ -0,0 +1,47 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m160713_053747_create_tables extends Migration
+{
+    public function Up()
+    {
+        $this->createTable('leaders', [
+            'LCODE' => Schema::TYPE_PK,
+            'CODE' => Schema::TYPE_INTEGER,
+            'FNAME' => Schema::TYPE_STRING,
+            'NAME' => Schema::TYPE_STRING,
+            'PATRONYMIC' => Schema::TYPE_STRING,
+            'EMAIL' => Schema::TYPE_STRING,
+            'PHONE' => Schema::TYPE_STRING
+        ]);
+
+        $this->createTable('enterprises', [
+            'CODE' => Schema::TYPE_PK,
+            'TITLE' => Schema::TYPE_STRING ,
+            'REGION' => Schema::TYPE_STRING ,
+            'CITY' => Schema::TYPE_STRING ,
+            'ADDRESS' => Schema::TYPE_STRING ,
+        ]);
+    }
+
+
+    public function down()
+    {
+        $this->dropTable('enterprises');
+        $this->dropTable('leaders');
+
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
